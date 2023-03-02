f8d9225113f9b4d2cd803145a0bdbc0c10a80e64
devopsrange <55711122+devopsrange@users.noreply.github.com>
2023-03-02 10:20:45 +0300

    Update counter_with_logging.sh

--- counter_with_logging.sh
+++ counter_with_logging.sh
@@ -9,18 +9,18 @@ do
   count=$(wc -l < $log_file)
   if [ $count -ge 20 ]
   then
-    echo "$(date +"%Y-%m-%d %H:%M:%S"): Record count is $count, killing $script_file" >> $killer_log
     pkill -f $script_file
     deletion_status=$?
-    echo "" > $log_file
-    deletion_status=$((deletion_status + $?))
     if [ $deletion_status -eq 0 ]
     then
+      rm $log_file
+      echo "$(date +"%Y-%m-%d %H:%M:%S"): Deleted $log_file" >> $killer_log
+      touch $log_file
       echo "$(date +"%Y-%m-%d %H:%M:%S"): Emptied $log_file" >> $killer_log
       nohup ./$script_file > $log_file 2>&1 &
       echo "$(date +"%Y-%m-%d %H:%M:%S"): Started $script_file" >> $killer_log
     else
-      echo "$(date +"%Y-%m-%d %H:%M:%S"): Failed to empty $log_file" >> $killer_log
+      echo "$(date +"%Y-%m-%d %H:%M:%S"): Failed to delete $log_file" >> $killer_log
     fi
   fi
   sleep 10
